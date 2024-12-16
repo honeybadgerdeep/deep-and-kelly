d26cf2817815b6e723eafd9494fa7f0870ca1422
honeybadgerdeep <honeybadgerdeep@gmail.com>
2024-12-15 20:15:45 -0800

    feat: registry consolidated to single link on WithJoy

--- src/routes/registry/+page.svelte
+++ src/routes/registry/+page.svelte
@@ -7,18 +7,13 @@
         <h1>Wedding Registry</h1>
     </header>
     <h3>
-        The greatest gift of all is your presence on our special day! However, should you wish to send gifts our way, there are two ways!
+        The greatest gift of all is your presence on our special day! However, should you wish to send gifts our way, below is the link to our registry!
     </h3>
     <div class="card-container">
-        <a target='_blank' href='https://www.amazon.com/wedding/registry/OO5Z52RMDHZA'>
-            <div class='card'>
-                <h2>Wedding Registry</h2>
-            </div>    
-        </a>
         <a target='_blank' href='https://withjoy.com/deep-and-sethi/registry'>
             <div class='card'>
-                <h2>Cash Fund</h2>
-            </div>    
+                <h2>Wedding Registry</h2>
+            </div>
         </a>
     </div>
 </div>
